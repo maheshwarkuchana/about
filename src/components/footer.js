2909bdbc9d0a7f5e635dfba0031883dc5a82c640
Maheshwar Kuchana <kmaheshwar1998@gmail.com>
2020-05-25 08:44:16 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,10 +102,10 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://github.com/maheshwarkuchana"
           target="_blank"
           rel="nofollow noopener noreferrer">
-          <div>Designed by Brittany Chiang</div>
+          <div>Designed by Maheshwar</div>
 
           {/* {githubInfo.stars && githubInfo.forks && (
             <StyledGitHubInfo>
